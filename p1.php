3e94c39b4be1b828ab73f199e2fac522c48b880b
programmerdiego <programmerdiego@gmail.com>
2018-09-14 16:59:25 -0400

    practice one

--- p1.php
+++ p1.php
@@ -1,5 +1,10 @@
+ <!––
+Program Name: P1 
+Description:  Practice PHP inside HTML
+Author:       Diego Sanchez
+ ––> 
 <?php
-$airpupTag = 'I luv puppies';
+$airpupTag = 'I love music';
 $yearFounded = '2015';
 ?>
 <h2>
